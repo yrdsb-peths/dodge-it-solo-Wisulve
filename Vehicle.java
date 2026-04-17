c9328de556940101180817e9772a868e467bc7b2
Wisulve-328 <440042792@gapps.yrdsb.ca>
2026-04-16 20:46:46 -0400

    reset vehicle

--- Vehicle.java
+++ Vehicle.java
@@ -4,5 +4,17 @@ public class Vehicle extends Actor
     public void act()
     {
         move(-3);
+        if(getX() <= 0){
+            resetVehicle();
+        }
+    }
+    public void resetVehicle(){
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0){
+            setLocation(600, 100);
+        }else{
+            setLocation(600, 300);
+        }
+        
     }
 }
